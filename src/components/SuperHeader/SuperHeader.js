b9c172b0a8105b7e2952b66826f847fd901369fc
rafaelalma <rafaelalma92@gmail.com>
2023-05-24 21:07:50 +0200

    feat(Superheader): style

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
-import { COLORS } from '../../constants';
+import { COLORS } from '../../constants'
 
-import SearchInput from '../SearchInput';
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
+import SearchInput from '../SearchInput'
+import UnstyledButton from '../UnstyledButton'
+import Icon from '../Icon'
 
 const SuperHeader = () => {
   return (
@@ -19,18 +19,23 @@ const SuperHeader = () => {
         <Icon id="shopping-bag" strokeWidth={1} />
       </UnstyledButton>
     </Wrapper>
-  );
-};
+  )
+}
 
 const Wrapper = styled.div`
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
-`;
+  padding: 12px 32px;
+  display: flex;
+  gap: 24px;
+  align-items: center;
+`
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
-`;
+  margin-right: auto;
+`
 
 const HelpLink = styled.a`
   color: inherit;
@@ -40,6 +45,6 @@ const HelpLink = styled.a`
   &:not(:focus-visible) {
     outline: none;
   }
-`;
+`
 
-export default SuperHeader;
+export default SuperHeader
