63f2d1fd7b9f7c90c42e5a7d65702e291da47a7d
rafaelalma <rafaelalma92@gmail.com>
2023-05-28 21:37:25 +0200

    style(ShoeCard): flags

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -33,19 +33,27 @@ const ShoeCard = ({
 
   return (
     <Link href={`/shoe/${slug}`}>
-      <Wrapper>
+      <article>
         <ImageWrapper>
           <Image alt="" src={imageSrc} />
+          {variant === 'default' ? null : (
+            <Flag variant={variant}>
+              {variant === 'on-sale' ? 'Sale' : 'Just Released!'}
+            </Flag>
+          )}
         </ImageWrapper>
         <Spacer size={12} />
         <Row>
           <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
+          <Price variant={variant}>{formatPrice(price)}</Price>
         </Row>
         <Row>
           <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
+          {variant === 'on-sale' ? (
+            <SalePrice>{formatPrice(salePrice)}</SalePrice>
+          ) : null}
         </Row>
-      </Wrapper>
+      </article>
     </Link>
   )
 }
@@ -54,8 +62,6 @@ const Link = styled.a`
   text-decoration: none;
   color: inherit;
 `
-
-const Wrapper = styled.article``
 
 const ImageWrapper = styled.div`
   position: relative;
@@ -67,19 +73,43 @@ const Image = styled.img`
   border-radius: 16px 16px 4px 4px;
 `
 
+const Flag = styled.div`
+  height: 32px;
+  background-color: ${(p) =>
+    p.variant === 'on-sale' ? COLORS.primary : COLORS.secondary};
+  border-radius: 2px;
+  width: fit-content;
+  padding: 0 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: ${WEIGHTS.medium};
+  font-size: ${14 / 16}rem;
+  color: ${COLORS.white};
+  line-height: 1rem;
+  position: absolute;
+  top: 12px;
+  right: -4px;
+`
+
 const Row = styled.div`
   font-size: 1rem;
   display: flex;
   align-items: baseline;
+  justify-content: space-between;
 `
 
 const Name = styled.h3`
   font-weight: ${WEIGHTS.medium};
   color: ${COLORS.gray[900]};
-  margin-right: auto;
 `
 
-const Price = styled.span``
+const Price = styled.span`
+  color: ${(p) =>
+    p.variant === 'on-sale' ? COLORS.gray[700] : COLORS.gray[900]};
+  text-decoration-line: ${(p) =>
+    p.variant === 'on-sale' ? 'line-through' : 'none'};
+`
 
 const ColorInfo = styled.p`
   color: ${COLORS.gray[700]};
